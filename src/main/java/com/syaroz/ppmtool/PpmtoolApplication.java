b4fb2623c78e8a6ebe997f7c98f495d747bc89cf
syaroz <syahrul.rozi.30@gmail.com>
2019-04-01 15:13:33 +0700

    initialize project

---
+++ src/main/java/com/syaroz/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.syaroz.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtoolApplication.class, args);
+    }
+
+}
